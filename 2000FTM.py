672e5f33d75714991b7f5e6a937111a6c11d5713
Niceck <126089681+Niceck@users.noreply.github.com>
2023-03-31 13:01:44 +0800

    Update 2000FTM.py

--- 2000FTM.py
+++ 2000FTM.py
@@ -10,12 +10,12 @@ api_secret = '7JJ079zKEEeO6wZnSHhxDRkx81CG0AFvl7450PixmSl9UP0F3yoMlupCRJGtz5KK'
 # 初始化币安客户端
 client = Client(api_key=api_key, api_secret=api_secret)
 # 交易对和K线周期
-symbol = 'XRPUSDT'
-interval = Client.KLINE_INTERVAL_1MINUTE
-FIXED_USDT_AMOUNT = 1
+symbol = 'FTMUSDT'
+interval = Client.KLINE_INTERVAL_5MINUTE
+FIXED_USDT_AMOUNT = 10
 LEVERAGE = 50
 TIME_GAP = 5
-STOP_LOSS_PERCENTAGE = 0.01
+STOP_LOSS_PERCENTAGE = 0.07
 quantity = 0
 def get_latest_market_price(symbol):
     try:
@@ -194,12 +194,12 @@ while True:
         deviation = (latest_market_price - MA7) / MA7
         deviation = deviation * 100.0
         print(f"市价: {latest_market_price:.4f}, 乖离率: {deviation:.2f}")
-        if prev_close_price and prev_close_price > max(ma7,ma14,ma28) and latest_market_price > max(MA7,MA14,MA28) and deviation < 0.2:
+        if prev_close_price and prev_close_price > max(ma7,ma14,ma28) and latest_market_price > max(MA7,MA14,MA28) and deviation < 0.15:
             # 当前趋势为上涨，开多头仓位
             open_position(Client.SIDE_BUY)
             last_print_time = time.time()
             print(f"多头趋势")
-        elif prev_close_price and prev_close_price < min(ma7,ma14,ma28) and latest_market_price < max(MA7,MA14,MA28) and deviation > -0.2:
+        elif prev_close_price and prev_close_price < min(ma7,ma14,ma28) and latest_market_price < max(MA7,MA14,MA28) and deviation > -0.15:
             # 当前趋势为下跌，开空头仓位
             open_position(Client.SIDE_SELL)
             last_print_time = time.time()
@@ -208,8 +208,8 @@ while True:
         if position:
             position_side = position['position']['positionSide']
             print(f"持仓方向：{position_side}")
-            if (position_side == 'LONG' and prev_close_price < ma7 or deviation > 0.5 or deviation < -0.2) or (
-                    position_side == 'SHORT' and prev_close_price > ma7 or deviation < -0.5 or deviation > 0.2):
+            if (position_side == 'LONG' and prev_close_price < ma7 or deviation > 0.6 or deviation < -0.2) or (
+                    position_side == 'SHORT' and prev_close_price > ma7 or deviation < -0.6 or deviation > 0.2):
                 close_position(symbol, position_side, prev_close_price, deviation)
                 cancel_all_orders(symbol)
             else:
