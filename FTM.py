16ade5573ce5526f796738903aea74c1a43c06cf
Niceck <126089681+Niceck@users.noreply.github.com>
2024-01-04 00:56:37 +0800

    Update FTM.py

--- FTM.py
+++ FTM.py
@@ -4,6 +4,7 @@ from binance.client import Client
 import talib
 import numpy as np
 from binance.exceptions import BinanceAPIException
+
 # 填写自己的币安API Key和Secret
 api_key = 'GjHUW3DZFizTt7rfuoI58xebWaMtDOMxjWJjjG34f8wiJRdUjE7l24b8L046StvI'
 api_secret = '7JJ079zKEEeO6wZnSHhxDRkx81CG0AFvl7450PixmSl9UP0F3yoMlupCRJGtz5KK'
@@ -17,6 +18,7 @@ LEVERAGE = 1
 STOP_LOSS_PERCENTAGE = 0.015
 quantity = 0
 
+
 # 获取最新市场价格
 def get_latest_market_price(symbol):
     try:
@@ -26,15 +28,18 @@ def get_latest_market_price(symbol):
         print(f"获取市价失败，错误信息为：{str(e)}")
         return 0
 
+
 # 获取前一根K线的收盘价
 def get_previous_close_price():
     klines = client.futures_klines(symbol=symbol, interval=interval, limit=2)
     return float(klines[-2][4]) if len(klines) > 1 else 0
 
+
 def get_ma(period, limit=0):
     klines = client.futures_klines(symbol=symbol, interval=interval, limit=period + limit)
     close_prices = np.array([float(kline[4]) for kline in klines[:-limit] or klines])
     return talib.SMA(close_prices, timeperiod=period)[-1]
+
 
 # 获取技术指标：ADX, DI+, DI-, MACD
 def get_technical_indicators():
@@ -49,6 +54,7 @@ def get_technical_indicators():
     macd = talib.MACD(close_prices, fastperiod=12, slowperiod=26, signalperiod=9)[0][-1]
     return adx, plus_di, minus_di, macd
 
+
 # 检查是否持仓
 def has_position(symbol):
     try:
@@ -57,11 +63,13 @@ def has_position(symbol):
         threshold = 1e-6
         for position in positions:
             if position['symbol'] == symbol and abs(float(position['positionAmt'])) > threshold:
-                return {'positionSide': 'LONG' if float(position['positionAmt']) > 0 else 'SHORT', 'amount': abs(float(position['positionAmt']))}
+                return {'positionSide': 'LONG' if float(position['positionAmt']) > 0 else 'SHORT',
+                        'amount': abs(float(position['positionAmt']))}
         return None
     except Exception as e:
         print(f"检查持仓失败：{e}")
         return None
+
 
 def close_position(symbol, position_side):
     try:
@@ -87,6 +95,7 @@ def close_position(symbol, position_side):
         print(f"平仓失败：{e}")
         return False
 
+
 # 获取交易对信息
 def get_symbol_info(symbol):
     try:
@@ -97,6 +106,7 @@ def get_symbol_info(symbol):
     except Exception as e:
         print(f"获取交易对信息失败：{str(e)}")
         return None
+
 
 # 更新账户余额信息
 def get_account_balance():
@@ -112,6 +122,7 @@ def get_account_balance():
         print(f"获取账户余额失败：{str(e)}")
         return 0
 
+
 def get_price_change(symbol, interval, periods):
     klines = client.futures_klines(symbol=symbol, interval=interval, limit=max(periods) + 1)
     close_prices = np.array([float(kline[4]) for kline in klines])
@@ -119,7 +130,7 @@ def get_price_change(symbol, interval, periods):
     price_changes = {}
     for period in periods:
         if len(close_prices) > period:
-            change = (close_prices[-1] - close_prices[-1-period]) / close_prices[-1-period]
+            change = (close_prices[-1] - close_prices[-1 - period]) / close_prices[-1 - period]
             price_changes[period] = change
     return price_changes
 
@@ -128,6 +139,7 @@ def get_price_change(symbol, interval, periods):
 def adjust_precision(quantity, step_size):
     precision = int(round(-math.log(step_size, 10), 0))
     return round(quantity, precision)
+
 
 # 取消所有订单
 def cancel_all_orders(symbol):
@@ -140,6 +152,7 @@ def cancel_all_orders(symbol):
     except Exception as e:
         print(f"取消委托单失败：{e}")
         return False
+
 
 # 开仓函数
 def open_position(side):
@@ -196,17 +209,19 @@ def open_position(side):
         print(f"开仓失败：{e}")
         return False
 
+
 # 定义ATR计算函数
 def get_atr(period=7):
-    klines = client.futures_klines(symbol=symbol, interval=interval, limit=period+1)
+    klines = client.futures_klines(symbol=symbol, interval=interval, limit=period + 1)
     high_prices = np.array([float(kline[2]) for kline in klines])
     low_prices = np.array([float(kline[3]) for kline in klines])
     close_prices = np.array([float(kline[4]) for kline in klines])
     atr = talib.ATR(high_prices, low_prices, close_prices, timeperiod=period)[-1]
     return atr
 
+
 # 定义ATR阈值
-ATR_THRESHOLD = 0.005 # 示例值，您需要根据实际情况调整
+ATR_THRESHOLD = 0.005  # 示例值，您需要根据实际情况调整
 # 主逻辑代码
 while True:
     try:
@@ -229,18 +244,22 @@ while True:
         current_ma60 = get_ma(60)  # 当前MA60
 
         # 获取价格变化
-        price_changes = get_price_change(symbol, interval, [5,10,20, 60])
-        print(f"FTM----{price_changes:.4f}")
-        
+        price_changes = get_price_change(symbol, interval, [5, 10, 20, 60])
+        print(f"FTM----{price_changes}")
+
         # 获取ATR和技术指标
         atr = get_atr(5)
         adx, plus_di, minus_di, macd = get_technical_indicators()
-        print(f"atr---:{atr:.4f},---adx---{adx:.4f},{plus_di:.4f},{minus_di:.4f}")
+        print()
+        print(f"FTM----atr---:{atr:.4f},---adx---{adx:.4f},{plus_di:.4f},{minus_di:.4f}")
+        print()
 
         # 检查买入条件
         if not has_position(symbol) and all([
-            prev_close_price > prev_ma5, prev_close_price > prev_ma10, prev_close_price > prev_ma20, prev_close_price > prev_ma60, 
-            latest_price > current_ma5, latest_price > current_ma10, latest_price > current_ma20, latest_price > current_ma60,
+            prev_close_price > prev_ma5, prev_close_price > prev_ma10, prev_close_price > prev_ma20,
+            prev_close_price > prev_ma60,
+            latest_price > current_ma5, latest_price > current_ma10, latest_price > current_ma20,
+            latest_price > current_ma60,
             current_ma5 > current_ma10, current_ma10 > current_ma20, atr > ATR_THRESHOLD, macd > 0,
             price_changes[5] > 0, price_changes[10] > 0, price_changes[20] > 0, price_changes[20] > 60,
             atr > ATR_THRESHOLD, adx > 20, plus_di > minus_di]):
@@ -248,10 +267,12 @@ while True:
 
         # 检查卖出条件
         elif not has_position(symbol) and all([
-            prev_close_price < prev_ma5, prev_close_price < prev_ma10, prev_close_price < prev_ma20, prev_close_price < prev_ma60, 
-            latest_price < current_ma5, latest_price < current_ma10, latest_price < current_ma20, latest_price < current_ma60,
+            prev_close_price < prev_ma5, prev_close_price < prev_ma10, prev_close_price < prev_ma20,
+            prev_close_price < prev_ma60,
+            latest_price < current_ma5, latest_price < current_ma10, latest_price < current_ma20,
+            latest_price < current_ma60,
             current_ma5 < current_ma10, current_ma10 < current_ma20, atr > ATR_THRESHOLD, macd < 0,
-            price_changes[5] <  0, price_changes[10] <  0, price_changes[20] < 0, price_changes[20] < 60,
+            price_changes[5] < 0, price_changes[10] < 0, price_changes[20] < 0, price_changes[20] < 60,
             atr > ATR_THRESHOLD, adx > 20, plus_di < minus_di]):
             open_position(Client.SIDE_SELL)
 
@@ -264,7 +285,7 @@ while True:
             # 对于多头持仓
             if position_side == 'LONG':
                 if all([
-                    latest_price < current_ma5, latest_price < current_ma10, 
+                    latest_price < current_ma5, latest_price < current_ma10,
                     prev_close_price < prev_ma5, prev_close_price < prev_ma10,
                     plus_di < minus_di]):
                     close_position(symbol, position_side)
@@ -273,7 +294,7 @@ while True:
             # 对于空头持仓
             elif position_side == 'SHORT':
                 if all([
-                    latest_price > current_ma5, latest_price > current_ma10, 
+                    latest_price > current_ma5, latest_price > current_ma10,
                     prev_close_price > prev_ma5, prev_close_price > prev_ma10,
                     plus_di > minus_di]):
                     close_position(symbol, position_side)
