9547e934d6a95f3f5ecd039446d1ab0f86c37404
Niceck <126089681+Niceck@users.noreply.github.com>
2024-01-05 00:10:11 +0800

    Update FTM.py

--- FTM.py
+++ FTM.py
@@ -11,10 +11,10 @@ api_secret = '7JJ079zKEEeO6wZnSHhxDRkx81CG0AFvl7450PixmSl9UP0F3yoMlupCRJGtz5KK'
 # 初始化币安客户端
 client = Client(api_key=api_key, api_secret=api_secret)
 # 交易对和K线周期
-symbol = 'FTMUSDT'
+symbol = 'ORDIUSDT'
 interval = Client.KLINE_INTERVAL_1MINUTE
-FIXED_USDT_AMOUNT = 20
-LEVERAGE = 1
+FIXED_USDT_AMOUNT = 50
+LEVERAGE = 20
 STOP_LOSS_PERCENTAGE = 0.015
 quantity = 0
 
