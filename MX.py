6f830120b7f6fa71dbab434e59459c995d74f811
Niceck <126089681+Niceck@users.noreply.github.com>
2024-01-04 10:22:57 +0800

    Update and rename DDD.py to MX.py

--- MX.py
+++ MX.py
@@ -237,11 +237,11 @@ if __name__ == "__main__":
                 print()
                 order_response = place_order(symbol, str(latest_price), quantity, trade_type)
 
-            elif tao_balance < float(round(amount_in_usdt / latest_price, 3)) and \
+            elif tao_balance < float(round(5 / latest_price, 3)) and \
                  previous_close > max(ma_values.values()) and \
                  ma_values[5] > ma_values[10] and \
                  price_changes[20] > 0 and price_changes[60] > 0 and \
-                 adx_value > 20 and plus_di > minus_di and atr_value > 0.005
+                 adx_value > 20 and plus_di > minus_di and atr_value > 0.001
                  print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
                 # 执行买入操作
                 quantity = str(round(amount_in_usdt / latest_price - tao_balance,3))  # 计算买入数量
